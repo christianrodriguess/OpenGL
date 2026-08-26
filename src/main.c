21c24ae06072ab9fd7d839b2f1bd549ac3e723e5
Christian <christianmrodriguess@gmail.com>
2026-08-26 19:13:58 -0300

    desenhando letras

--- src/main.c
+++ src/main.c
@@ -1,29 +1,55 @@
 #include <GL/freeglut.h>
 
+void desenhaLetraC(){
+    glBegin(GL_LINE_STRIP);
+        glVertex2f(3, 5);
+        glVertex2f(2, 5);
+        glVertex2f(1, 4);
+        glVertex2f(1, 2);
+        glVertex2f(2, 1);
+        glVertex2f(3, 1);
+    glEnd();
+}
+
+void desenhaLetraH(){
+    glBegin(GL_LINES);
+        glVertex2f(5, 1);
+        glVertex2f(5, 5);
+        glVertex2f(5, 3);
+        glVertex2f(7, 3);
+        glVertex2f(7, 1);
+        glVertex2f(7, 5);
+    glEnd();
+}
+
+void desenhaLetraR(){
+    glBegin(GL_LINE_STRIP);
+        glVertex2f(9, 1);
+        glVertex2f(9, 5);
+        glVertex2f(11, 5);
+        glVertex2f(11, 3);
+        glVertex2f(9, 3);
+        glVertex2f(11, 1);
+    glEnd();
+}
+
 void desenha(void) {
     glClear( GL_COLOR_BUFFER_BIT );
 
-    gluOrtho2D(-3, 3, -3, 3);
+    gluOrtho2D(0, 13, 0, 10);
 
-    glBegin(GL_LINES);
-        glVertex2f(-2, 0);
-        glVertex2f(2, 0);
-    glEnd();
-
-    glBegin(GL_LINE_STRIP);
-        glVertex2f(0.25, 0.25);
-        glVertex2f(0.75, 0.75);
-        glVertex2f(0.30, 0.45);
-        glVertex2f(0, 0);
-    glEnd();
+    desenhaLetraC();
+    desenhaLetraH();
+    desenhaLetraR();
 
     glFlush();  
 }
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA );
+    glutInitDisplayMode( GLUT_SINGLE | GLUT_RGBA );
     glutInitWindowSize(800, 600);
+    glutInitWindowPosition(0, 0);
     glutCreateWindow("Minha Janela OpenGL");
 
     glutDisplayFunc(desenha);
