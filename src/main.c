b5d62c2f2a2543c343e06b735749145b5dad3ced
Christian <christianmrodriguess@gmail.com>
2026-08-26 12:03:24 -0300

    desenhando linhas

--- src/main.c
+++ src/main.c
@@ -1,7 +1,22 @@
 #include <GL/freeglut.h>
 
 void desenha(void) {
-    glClear( GL_COLOR_BUFFER_BIT ); 
+    glClear( GL_COLOR_BUFFER_BIT );
+
+    gluOrtho2D(-3, 3, -3, 3);
+
+    glBegin(GL_LINES);
+        glVertex2f(-2, 0);
+        glVertex2f(2, 0);
+    glEnd();
+
+    glBegin(GL_LINE_STRIP);
+        glVertex2f(0.25, 0.25);
+        glVertex2f(0.75, 0.75);
+        glVertex2f(0.30, 0.45);
+        glVertex2f(0, 0);
+    glEnd();
+
     glFlush();  
 }
 
